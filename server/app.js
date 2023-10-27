ddbfb8086700ffe76aabe1acabece15ce187b3fa
Kevin Lopez <kevinavs.2001@gmail.com>
2023-10-27 09:52:21 -0500

    Subida de imagen con errores

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
@@ -25,9 +26,23 @@ app.get('/suma/:num1/:num2', (req, res) => {
 
 // Servicio POST para recibir datos
 app.post('/guardar', (req, res) => {
-    const datos = req.body; // Aquí puedes acceder al cuerpo de la solicitud POST
-    console.log('Datos recibidos:', datos);
-    res.send('Datos recibidos con éxito.');
+  const datos = req.body;
+  console.log('Datos recibidos:', datos);
+  const nombreArchivo = datos.nombre;
+  const contenidoBinario = fs.readFileSync(datos.rutaArchivo);
+  const consulta = 'INSERT INTO imagenes (nombre, imagen) VALUES (?, ?)';
+  connection.query(consulta, [nombreArchivo, contenidoBinario], (error, resultados) => {
+    if (error) {
+      console.error('Error en la inserción: ' + error.stack);
+      return;
+    }
+
+    console.log('Imagen insertada con éxito.');
+
+    // Cierra la conexión a la base de datos
+    connection.end();
+  });
+  res.send('Datos recibidos con éxito.');
 });
 
 app.listen(port, () => {
